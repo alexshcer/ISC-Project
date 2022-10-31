3125f1fac0f018043437ca7e7c71e1d0412f7db2
alexshcer <alexmerser591@gmail.com>
2022-10-31 20:43:17 +0530

    Fixes

--- Program-24/DiaMat.java
+++ Program-24/DiaMat.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class rearrange {
+public class DiaMat{
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("ENTER MATRIX SIZE (M): ");
